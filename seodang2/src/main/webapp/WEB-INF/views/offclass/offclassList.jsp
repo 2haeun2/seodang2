5564042163b119fb1a986a2d1362397bef81c5fa
soyeon_lim <lsy96060@naver.com>
2022-03-24 16:14:32 +0900

    오프라인 클래스 등록 리스트

--- seodang2/src/main/webapp/WEB-INF/views/offclass/offclassList.jsp
+++ seodang2/src/main/webapp/WEB-INF/views/offclass/offclassList.jsp
@@ -3,10 +3,31 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <link rel="stylesheet" href="${pageContext.request.contextPath}/resources/css/offclass.css">
 <div class="container">
-	<h4>오프라인 CLASS</h4>
+	<h3>오프라인 CLASS</h3>
 	<c:if test="${!empty session_user_num && session_user_auth>=3}">
 	<div class="align-right">
 		<input type="button" value="CLASS 등록" onclick="location.href='offclassOpen.do'" class="btn btn-outline-secondary">
 	</div>
+	</c:if>
+	<c:if test="${count==0 }">
+		<div class="result-dispaly">등록된 클래스가 없습니다.</div>
+	</c:if>
+	<c:if test="${count>0 }">
+	<div class="item-space">
+		<c:forEach var="item" items="${list }">
+			<div class="horizontal-area">
+				<div>${item.category_num }</div>
+				<a href="offclassDetail.do?off_num=${item.off_num }">
+					<c:if test="${!empty item.img_file }">
+						<img class="list-img" src=${item.img_file }>
+					</c:if>
+					<span>${item.off_name }</span>
+					<br>
+					<span><b>${item.off_price }</b></span>
+				</a>
+			</div>
+		</c:forEach>
+	</div>
+	<div>${pagingUtil }</div>
 	</c:if>
 </div>
--- seodang2/src/main/webapp/resources/js/offclass.time.js
+++ seodang2/src/main/webapp/resources/js/offclass.time.js
@@ -1,6 +1,17 @@
 $(function(){
+	//이 페이지에 진입시 무조건 스토리지 초기화
+	init();
 	
-	
+	//로컬스토리지 저장할 배열
+	let timetable=[];
+	//로컬스토리지에 저장
+	function savetimetable(){
+		localStorage.setItem("timetable",JSON.stringify(timetable));
+	}
+	//로컬스토리지 초기화
+	function init(){
+		localStorage.removeItem("timetable");
+	}
 	
 	//등록 버튼을 눌렀을 때 목록이 출력이 되도록
 	$('#time_register').click(function(){
@@ -26,7 +37,12 @@ $(function(){
 		let time_start = $('#time_start').val();
 		let time_end = $('#time_end').val();
 		
-		let timeUI  = '<div class="timetable">';
+		let data = {time_date:time_date, time_start:time_start, time_end:time_end};
+		timetable.push(data);
+		savetimetable();
+		let id = timetable.length-1;
+		
+		let timeUI  = '<div class="timetable" id="'+id+'">';
 			timeUI += '<div class="align-right">';
 			timeUI += '	<input type="button" value="X" id="delete-btn" class="btn-nopadding">';
 			timeUI += '</div>'
@@ -46,9 +62,35 @@ $(function(){
 		$('#time_start').val('');
 		$('#time_end').val('');
 	}
+	//삭제 버튼 클릭시 지우기
 	$(document).on('click','#delete-btn',function(){
+		divId = JSON.stringify($(this).parents('.timetable').attr('id'));
 		$(this).parents('.timetable').remove();
+		timetable.splice(divId,1);
+		alert(JSON.stringify(timetable));
+		localStorage.setItem("timetable",JSON.stringify(timetable));
 	})
 	
+/*	//submit 버튼 클릭시
+	$('#open_form').submit(function(){
+		let time = localStorage.getItem("timetable");
+		let data = $(this).serialize();
+		alert(time);
+		
+		$.ajax({
+			type:'post',
+			data:{time:timtable,data:data},
+			url:'offclassOpen.do',
+			dataType:'json',
+			cache:false,
+			timeout:30000,
+			success:function(param){
+				
+			},
+			error:function(){
+				alert('네트워크 오류 발생');
+			}
+		});
+	});*/
 	
 })
