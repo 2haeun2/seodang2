40d1a01d4369fbbb0b46bf690d7d8f5edb30d0cf
eunnnnn1996 <eunnnnn1996@gmail.com>
2022-03-24 16:12:47 +0900

    중간 점검 커밋

--- seodang2/src/main/java/kr/spring/onclass/controller/OstarController.java
+++ seodang2/src/main/java/kr/spring/onclass/controller/OstarController.java
@@ -1,6 +1,7 @@
 package kr.spring.onclass.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,9 +13,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import kr.spring.onclass.service.OnclassService;
+import kr.spring.onclass.vo.OnclassVO;
 import kr.spring.onclass.vo.OstarVO;
+import kr.spring.util.PagingUtil;
 
 @Controller
 public class OstarController {
@@ -25,10 +34,41 @@ public class OstarController {
 		return new OstarVO();
 	}
 	@GetMapping("/onclass/qnaList.do")
-	public String qnaForm(Model model,OstarVO ostarVO,int on_num,HttpServletRequest request) {
+	public ModelAndView process(
+			@RequestParam(value="pageNum",defaultValue="1")
+			int currentPage,
+			@RequestParam(value="keyfield",defaultValue="")
+			String keyfield,
+			@RequestParam(value="keyword",defaultValue="")
+			String keyword, int on_num,OstarVO ostarVO,HttpSession session) {
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("keyfield", keyfield);
+		map.put("keyword", keyword);
+		
+		Integer user_num = (Integer)session.getAttribute("session_user_num");
 		ostarVO.setOn_num(on_num);
-		model.addAttribute("list",onclassService.listAll());
-		return "qnaList";
+		ostarVO.setUser_num(user_num);
+		
+		int count = onclassService.selectRowCount(map);
+		
+		PagingUtil page = new PagingUtil(keyfield,keyword,
+                currentPage,count,6,10,"qnaList.do");
+
+		map.put("start",page.getStartCount());
+		map.put("end", page.getEndCount());
+
+		List<OstarVO> list = null;
+			if(count > 0) {
+				list = onclassService.listALL(map);
+			}
+
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("qnaList");
+		mav.addObject("count", count);
+		mav.addObject("list",list);
+		mav.addObject("pagingHtml", page.getPagingHtml());
+
+		return mav;
 	}
 	//별점 처리
 	@PostMapping("/onclass/qnaList.do")
@@ -36,6 +76,7 @@ public class OstarController {
 		Integer user_num = (Integer)session.getAttribute("session_user_num");
 		ostarVO.setUser_num(user_num);
 		onclassService.insertqna(ostarVO);
-		return "qnaList";
+
+		return "redirect:/onclass/qnaList.do?on_num="+on_num;
 	}
 }
--- seodang2/src/main/java/kr/spring/onclass/service/OnclassService.java
+++ seodang2/src/main/java/kr/spring/onclass/service/OnclassService.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.annotations.Param;
 import kr.spring.onclass.vo.OnclassVO;
 import kr.spring.onclass.vo.OnlikeVO;
 import kr.spring.onclass.vo.OstarVO;
+import kr.spring.qna.vo.OqnaReplyVO;
 
 
 
@@ -19,19 +20,20 @@ public interface OnclassService {
 	public List<OnclassVO> hitList(Map<String,Object> map);
 	public OnclassVO getOnclass(int num);
 	public void updateOnclass(OnclassVO onclassVO);
-	public void deleteOnclass(int on_num);
+	public void deleteOnclass(Integer on_num);
 	public OnclassVO selectOnclass(Integer on_num);
 	public void updateHit(Integer on_num);
 	
 	//찜 누럴ㅆ는지 확인
-	public OnlikeVO selectLike(Integer user_num,int on_num);
+	public OnlikeVO selectLike(Integer user_num,Integer on_num);
 	public void deleteLike(int onlike_num);
-	public void insertLike(Integer user_num,int on_num);
+	public void insertLike(Integer user_num,Integer on_num);
 	
 	//평점
 	public OstarVO selectQna(Integer user_num);
 	public void insertqna(OstarVO ostarvo);
 	public int avgQna(Integer on_num);
 	//리스트
-	public List<OstarVO> listAll();
+	public List<OstarVO> listALL(Map<String,Object> map);
+	
 }
